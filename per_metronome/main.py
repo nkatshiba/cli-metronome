4410f1ed24b70bb3d6e352bcd00b0748c5f3c5c4
nkatshiba <nkatshiba@proton.me>
2023-12-05 22:34:23 +0100

    hotfix(es)

--- per_metronome/main.py
+++ per_metronome/main.py
@@ -1,6 +1,6 @@
 import time
-import curses
 import pygame
+import urwid
 
 pygame.mixer.init()
 
@@ -8,54 +8,43 @@ beat_sound = pygame.mixer.Sound('sounds/high.wav')
 accent_sound = pygame.mixer.Sound('sounds/bright.wav')
 
 
-def main(stdscr):
-    # Set up the screen
-    curses.curs_set(0)
-    stdscr.nodelay(1)
-    stdscr.timeout(100)
+def query_bpm():
+    user_input = input("Please enter something: ")
+    print("You entered: " + user_input)
+    return user_input
 
-    # Initialize BPM
-    bpm = 120
+
+def main():
+    bpm = float(query_bpm())
     beat = 0
-
-    # Initialize the time for the next beat
     next_beat_time = time.time()
 
-    while True:
-        # Calculate the time to wait before playing the next sound
+    def on_key(key):
+        nonlocal bpm, beat, next_beat_time
+        if key == 'q':
+            raise urwid.ExitMainLoop()
+        elif key == '+':
+            bpm = min(bpm + 1, 300)
+        elif key == '-':
+            bpm = max(bpm - 1, 20)
+
+    def update(_loop, _data):
+        nonlocal bpm, beat, next_beat_time
         next_beat_time += 60.0 / bpm
-
-        # Display the current BPM and beat
-        stdscr.clear()
-        stdscr.addstr(0, 0, f"BPM: {bpm}")
-        stdscr.addstr(1, 0, f"Beat: {beat+1}")
-
-        # Play the beat
-        if beat == 0:
-            # Accent on the first beat
+        beat = (beat + 1) % 4  # Increment beat first, then apply modulo
+        if beat == 0:  # Now this will be true every four beats
             accent_sound.play()
         else:
             beat_sound.play()
+        text.set_text(f"BPM: {bpm}\nBeat: {beat + 1}")  # Display beat as 1-4 for user-friendliness
+        loop.set_alarm_at(next_beat_time, update)
 
-        # Advance to the next beat
-        beat = (beat + 1) % 4
-
-        # Check for key presses
-        c = stdscr.getch()
-
-        if c == ord('q'):
-            # Quit if 'q' is pressed
-            break
-        elif c == ord('+'):
-            # Increase BPM if '+' is pressed
-            bpm = min(bpm + 1, 300)
-        elif c == ord('-'):
-            # Decrease BPM if '-' is pressed
-            bpm = max(bpm - 1, 20)
-
-        # Wait for the next beat
-        time.sleep(max(0, next_beat_time - time.time()))
+    text = urwid.Text(f"BPM: {bpm}\nBeat: {beat}")
+    filler = urwid.Filler(text, 'top')
+    loop = urwid.MainLoop(filler, unhandled_input=on_key)
+    loop.set_alarm_at(next_beat_time, update)
+    loop.run()
 
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    main()
